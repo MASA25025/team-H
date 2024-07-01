b0d4c514c2931156d0dd91fcfb84b1679ea9a4b4
m_negishi <m_negishi@LAPTOP-C05BKDSV>
2024-07-01 11:34:49 +0900

    DAO

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,5 +1,25 @@
 package dao;
 
-public class DAO {
+import java.sql.Connection;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DAO {
+//	クラス変数　全体で1つだけ、共有データとして保持
+	static DataSource ds;
+
+	public Connection getConnection() throws Exception{
+//		最初の１回だけ実行したい
+		if(ds == null){
+
+//		データベースに接続
+		InitialContext ic=new InitialContext();
+		ds =(DataSource)ic.lookup(
+				"java:/comp/env/jdbc/Javasd");
+		}
+
+		return ds.getConnection();
+	}
 }
+
