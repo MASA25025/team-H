4692835db5ce407c41db57e9ee15c0c73cc8dd71
admin <admin@LAPTOP-7CHGMKTV>
2024-07-01 13:58:20 +0900

    Actionでエラー出てるからわかる人居たら治してほしい

--- src/FrontController.java
+++ src/FrontController.java
@@ -1,18 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="{}")
+@WebServlet(urlPatterns={""})
 public class FrontController extends HttpServlet{
-
-	public void doGet(HttpServletRequest req, HttpServletResponse resp){
-
+	public void  doPost(HttpServletRequest req, HttpServletResponse resp
+			)throws ServletException, IOException {
+		PrintWriter out=resp.getWriter();
+		try{
+			String path=req.getServletPath().substring(1);
+			String name=path.replace(".a", "A").replace('/', '.');
+			Action action=(Action)Class.forName(name).getDeclaredConstructor().newInstance();
+			String url=action.execute(req, resp);
+			req.getRequestDispatcher(url).forward(req, resp);
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace(out);
+		}
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp){
+	public void doGet(HttpServletRequest req, HttpServletResponse reqp)throws ServletException, IOException {
+		doGet(req, reqp);
 
 	}
-
-
 }
