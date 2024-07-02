785ac4d5261c002e19be15bee3c7abeedba22ea8
m_negishi <m_negishi@LAPTOP-C05BKDSV>
2024-07-02 12:10:58 +0900

    class_numDAO完成

--- src/dao/ClassNumDao.java
+++ src/dao/ClassNumDao.java
@@ -1,9 +1,36 @@
 package dao;
 
-public class ClassNumDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-	public ClassNumDao() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
+import bean.Class_Num;
+
+public class ClassNumDao extends DAO{
+
+		public List<Class_Num> Filter(String keyword) throws Exception {
+			List<Class_Num> list=new ArrayList<>();
+
+			Connection con=getConnection();
+
+			PreparedStatement st=con.prepareStatement(
+				"select * from CLASS_NUM where CLASS_NUM like ?");
+			st.setString(1,keyword);
+			ResultSet rs=st.executeQuery();
+
+			while (rs.next()) {
+				Class_Num p=new Class_Num();
+				p.setSchool_cd(rs.getString("school_cd"));
+				p.setClass_num(rs.getString("class_num"));
+				list.add(p);
+			}
+
+			st.close();
+			con.close();
+
+			return list;
+		}
 
 }
