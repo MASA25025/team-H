19d1292bae6b04938c0e2c69c864d7fb58cc36ee
m_negishi <m_negishi@LAPTOP-C05BKDSV>
2024-07-02 10:11:28 +0900

    ぴゃー

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -1,5 +1,15 @@
 package action;
 
-public class LoginAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import tool.Action;
+
+public class LoginAction extends Action {
+	public String execute(
+			HttpServletRequest request,HttpServletResponse response
+	) throws Exception{
+
+		return "login.jsp";
+	}
 }
