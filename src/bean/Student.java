838826aba63eebc4bb2207c8d84dad48ac88adda
admin <admin@LAPTOP-9M25HRLN>
2024-07-04 10:42:34 +0900

    修正

--- src/bean/Student.java
+++ src/bean/Student.java
@@ -8,48 +8,49 @@ public class Student implements Serializable {
 	//学生名
 	private String name;
 	//入学年度
-	private int ent_year;
+	private int entYear;
 	//クラス番号
-	private String class_num;
+	private String classNum;
 	//在学中フラグ
-	private boolean is_attend;
+	private boolean isAttend;
 	//学校コード
-	private String school_cd;
+	private School school ;
 	public String getNo() {
 		return no;
-	}
-	public void setNo(String no) {
-		this.no = no;
 	}
 	public String getName() {
 		return name;
 	}
+	public int getEntYear() {
+		return entYear;
+	}
+	public String getClassNum() {
+		return classNum;
+	}
+	public boolean isAttend() {
+		return isAttend;
+	}
+	public School getSchool() {
+		return school;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getEnt_year() {
-		return ent_year;
+	public void setEntYear(int entYear) {
+		this.entYear = entYear;
 	}
-	public void setEnt_year(int ent_year) {
-		this.ent_year = ent_year;
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
 	}
-	public String getClass_num() {
-		return class_num;
+	public void setAttend(boolean isAttend) {
+		this.isAttend = isAttend;
 	}
-	public void setClass_num(String class_num) {
-		this.class_num = class_num;
+	public void setSchool(School school) {
+		this.school = school;
 	}
-	public boolean isIs_attend() {
-		return is_attend;
-	}
-	public void setIs_attend(boolean is_attend) {
-		this.is_attend = is_attend;
-	}
-	public String getSchool_cd() {
-		return school_cd;
-	}
-	public void setSchool_cd(String school_cd) {
-		this.school_cd = school_cd;
-	}
-	
+
+
 }
