43e7390ce55b6657e9a16a52a0a13ab1cde1e1ae
admin <admin@LAPTOP-7CHGMKTV>
2024-07-10 20:56:15 +0900

    仮

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -9,7 +9,42 @@ import java.util.List;
 import bean.School;
 import bean.Student;
 
+
 public class StudentDao extends DAO {
+
+	// 学生情報を学生番号で取得するメソッド
+    public Student get(String no) {
+        String sql = "SELECT * FROM students WHERE student_number = ?";
+        Connection connection = getConnection();
+         	PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, no);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            //            resultSet.next();
+// 				学生情報を取得してStudentオブジェクトを作成
+//                int id = resultSet.getInt("id");
+//                String name = resultSet.getString("name");
+
+                // 他のカラムも同様に取得
+			// Productクラスをインスタンス化
+			Student p = new Student();
+			School q = new School();
+			// 値をセット
+			p.setEntYear(rs.getInt("id"));
+			p.setNo(rs.getString("name"));
+			p.setName(rs.getString("price"));
+			p.setClassNum(rs.getString("classnum"));
+			p.setAttend(rs.getBoolean("attend"));
+
+			q.setCd(rs.getString("cd"));
+			p.setSchool(q);
+            return ;
+
+
+
+    }
+
+
 
 	public List<Student> filter(School school, String entYear, String classNum, String isAttend ) throws Exception {
 		List<Student> studentlist = new ArrayList<Student>();
@@ -47,6 +82,8 @@ public class StudentDao extends DAO {
 
 		return studentlist;
 	}
+
+
 
 
 
