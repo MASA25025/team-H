6f11a251202e52e44eca5c598e12ef4c5278ad1f
admin <admin@LAPTOP-7CHGMKTV>
2024-07-12 06:10:16 +0900

    studentlistさわるよ

--- src/action/StudentListAction.java
+++ src/action/StudentListAction.java
@@ -1,34 +1,38 @@
 package action;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import	java.util.Map;;
+
 import bean.Student;
+import bean.Teacher;
+import dao.ClassNumDao;
 import dao.StudentDao;
 import tool.Action;
 
 public class StudentListAction extends Action {
-	public String execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
-		HttpSession session = request.getSession();
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp)throws Exception{
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-		String entYear = request.getParameter("entYear");
-		if (entYear == null) {entYear = "";}
-
-		String classNum = request.getParameter("classNum");
-		if (classNum == null) {classNum = "";}
-
-		String isAttend = request.getParameter("isAttend");
-		if (isAttend == null) {isAttend = "";}
-
-
-		List<Student> studentlist = new StudentDao().filter(null, entYear, classNum, isAttend);
-
-		session.setAttribute("studentlist", studentlist);
-
-		return "student_list.jsp";
-
+		String entYearStr="";
+		String classNum = "";
+		String isAttendStr = "";
+		int entYaer = 0;
+		boolean isAttend = false;
+		List<Student> students = null;
+		LocalDate todaysDate = LocalDate.now();
+		int yaer = todaysDate.getYear();
+		StudentDao sDao = new StudentDao();
+		ClassNumDao classNumDao = new ClassNumDao();
+		Map<String, String> errors = new HashMap<>();
 	}
+
 }
