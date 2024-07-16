01d444771efd84a7c367a226495ee3632c77db6a
admin <admin@LAPTOP-7CHGMKTV>
2024-07-16 10:59:04 +0900

    a

---
+++ src/dao/TestlistSutudentDao.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import bean.Student;
+import bean.TestListStudent;
+
+public class TestlistSutudentDao {
+
+	private String baleSql = "select";
+
+	public List<TestListStudent> postFilter(ResultSet rSet) {
+
+	}
+
+	public List<TestListStudent> filter(Student student) {
+
+	}
+
+}
