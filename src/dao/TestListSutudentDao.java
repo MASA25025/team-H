db6acab7b18a8102bee5dbd8fd3852c61bb96c60
admin <admin@LAPTOP-7CHGMKTV>
2024-07-16 11:31:02 +0900

    n

--- src/dao/TestListSutudentDao.java
+++ src/dao/TestListSutudentDao.java
@@ -6,12 +6,12 @@ import java.util.List;
 import bean.Student;
 import bean.TestListStudent;
 
-public class TestlistSutudentDao {
+public class TestListSutudentDao {
 
 	private String baleSql = "select";
 
 	public List<TestListStudent> postFilter(ResultSet rSet) {
-
+		
 	}
 
 	public List<TestListStudent> filter(Student student) {
