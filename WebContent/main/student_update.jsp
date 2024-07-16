b5955e6fcfccfe506888019219d2cfb21f69eeaa
admin <admin@LAPTOP-9M25HRLN>
2024-07-16 12:34:10 +0900

    (仮)

---
+++ WebContent/main/student_update.jsp
@@ -0,0 +1,51 @@
+<%@page contentType="text/html; charset=UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<c:import url="/base.jsp">
+
+<c:param name="title">得点管理システム</c:param>
+
+<c:param name="content">
+
+	<h2>学生情報変更</h2>
+
+	<form method="get">
+	<div>
+		<label>入学年度
+			<input value="${studens.ent_year}" readonly="readonly">
+		</label>
+	</div>
+	<div>
+		<label>学生番号
+			<input value="${studenst.no}" readonly="readonly">
+		</label>
+	</div>
+	<div>
+			<label>氏名
+			<input type="text" name="neme" value="${students.name}" maxlength="30">
+		</label>
+	</div>
+	<div>
+		<label>クラス
+		<select name="class_num">
+			<option value="${num}">-------</option>
+			<c:forEach var="num" items="${class_num_set}" ></c:forEach>
+				<option value="${num}" <c:if test="${num==class_num}">selected</c:if>>${year}</option>
+		</select>
+		</label>
+	</div>
+	<div>
+		<label>在学中
+			<input type="checkbox" name="si_attend" <c:if test="${!empty si_attend}">checked</c:if> />
+		</label>
+	</div>
+	<div>
+		<button>変更</button>
+	</div>
+
+		<a href="">戻る</a>
+	</form>
+
+</c:param>
+
+</c:import>
--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -13,64 +13,38 @@ import bean.Student;
 import bean.Subject;
 import bean.Test;
 
-public class TestDao extends DAO{
+public class TestDao extends DAO {
+    private String baseSql = "SELECT * FROM test WHERE school_cd = ?";
 
-//	public List<Test> filter(int ent_year,String class_num,String subject_name,int sub_cou) throws Exception {
-//		List<Test> list=new ArrayList<>();
-//		Connection con=getConnection();
-//
-//		PreparedStatement st=con.prepareStatement(
-//			"select ENT_YEAR,CLASS_NUM,NO,NAME from STUDENT where ENT_YEAR,CLASS_NUM like ?,?");
-//		st.setInt(1,ent_year);
-//		st.setString(1,class_num);
-//		ResultSet rs=st.executeQuery();
-//
-//		while (rs.next()) {
-//			Test p=new Test();
-//			p.setEntYear(rs.getInt("ent_year"));
-//			p.setClassNum(rs.getString("class_num"));
-//			p.setNo(rs.getString("no"));
-//			p.setName(rs.getString("name"));
-//			list.add(p);
-//		}
-//
-//		st.close();
-//		con.close();
-//
-//		return list;
-//	}
+    public Test get(Student student, Subject subject, School school, int no) throws Exception {
+        Test test = new Test();
 
-	private String baseSql = "select * from Test where school_cd =?";
+        Connection connection = getConnection();
 
+        PreparedStatement statement = null;
 
-	public Test get(Student student, Subject subject, School school, int no)throws  Exception{
-		Test test = new Test();
+        try {
+        	statement = connection.prepareStatement(
+                 baseSql + " AND student_no = ? AND subject_cd = ? AND no = ?");
 
-		Connection connection = getConnection();
+            statement.setString(1, school.getCd());
+            statement.setString(2, student.getNo());
+            statement.setString(3, subject.getCd());
+            statement.setInt(4, no);
 
-		PreparedStatement statement = null;
+            ResultSet rSet = statement.executeQuery();
 
-		try{
-			statement = connection.prepareStatement("");
-			statement.setString(1, );
-
-			ResultSet rSet = statement.executeQuery();
-			StudentDao studentDao = new StudentDao();
-			SchoolDao schoolDao = new SchoolDao();
-
-
-			if(rSet.next()){
-				student.setNo(rSet.getString("no"));
-				student.setName(rSet.getString("name"));
-				student.setEntYear(rSet.getInt("ent_yaer"));
-				student.setClassNum(rSet.getString("class_num"));
-				student.setAttend(rSet.getBoolean("is_attend"));
-				student.setSchool(schoolDao.get(rSet.getString("school_cd")));
-			}else{
-				student = null;
+            if (rSet.next()) {
+                test.setStudent(student);
+                test.setClassNum(rSet.getString("class_num"));
+                test.setSubject(subject);
+                test.setSchool(school);
+                test.setNo(rSet.getInt("no"));
+                test.setPoint(rSet.getInt("point"));
+            }else{
+				test = null;
 			}
-//
-		}catch (Exception e) {
+        } catch (Exception e) {
 			// TODO: handle exception
 			throw e;
 		}finally {
@@ -91,31 +65,165 @@ public class TestDao extends DAO{
 				}
 			}
 		}
-		return student;
-	}
 
-	public List<Test> postFilter(ResultSet rSet, School school) {
+        return test;
+    }
 
-	}
+    private List<Test> postFilter(ResultSet rSet, School school) throws Exception {
+        List<Test> list = new ArrayList<>();
 
-	public List<Test> filter(int entYear, String classNum, Subject subject, int num, School school) {
+        while (rSet.next()) {
+            Test test = new Test();
+            Student student = new Student();
+            Subject subject = new Subject();
 
-	}
+            student.setNo(rSet.getString("student_no"));
+            student.setSchool(school);
+            subject.setCd(rSet.getString("subject_cd"));
+            subject.setSchool(school);
 
-	public boolean save(List<Test> list) {
+            test.setStudent(student);
+            test.setClassNum(rSet.getString("class_num"));
+            test.setSubject(subject);
+            test.setSchool(school);
+            test.setNo(rSet.getInt("no"));
+            test.setPoint(rSet.getInt("point"));
 
-	}
+            list.add(test);
+        }
 
-	public boolean save(Test test, Connection connection) {
+        return list;
+    }
 
-	}
+    public List<Test> filter(int entYear, String classNum, Subject subject, int num, School school) throws Exception {
+        List<Test> list = new ArrayList<>();
 
-	public boolean delete(List<Test> list) {
+        String sql = baseSql + " AND ent_year = ? AND class_num = ? AND subject_cd = ? AND no = ? ORDER BY student_no ASC";
 
-	}
+        Connection connection = getConnection();
 
-	public boolean delete(Test test, Connection connection) {
+        PreparedStatement statement = null;
 
-	}
+        try{
+        	statement = connection.prepareStatement(sql);
+
+            statement.setString(1, school.getCd());
+            statement.setInt(2, entYear);
+            statement.setString(3, classNum);
+            statement.setString(4, subject.getCd());
+            statement.setInt(5, num);
+
+            ResultSet rSet = statement.executeQuery();
+            list = postFilter(rSet, school);
+        }catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		}finally {
+			if (statement !=null){
+				try{
+					statement.close();
+				}catch (SQLException sqle) {
+					// TODO: handle exception
+					throw sqle;
+				}
+			}
+			if (connection != null){
+				try{
+					connection.close();
+				}catch (SQLException sqle) {
+					// TODO: handle exception
+					throw sqle;
+				}
+			}
+		}
+
+        return list;
+    }
+
+    public boolean save(List<Test> list) throws Exception {
+        boolean result = true;
+
+        try (Connection connection = getConnection()) {
+            connection.setAutoCommit(false);
+
+            for (Test test : list) {
+                if (!save(test, connection)) {
+                    result = false;
+                    break;
+                }
+            }
+
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback();
+            }
+        }
+
+        return result;
+    }
+
+    private boolean save(Test test, Connection connection) throws Exception {
+        boolean result;
+        String sql;
+
+        if (get(test.getStudent(), test.getSubject(), test.getSchool(), test.getNo()) == null) {
+            sql = "INSERT INTO test (student_no, class_num, subject_cd, school_cd, no, point) VALUES (?, ?, ?, ?, ?, ?)";
+        } else {
+            sql = "UPDATE test SET class_num = ?, point = ? WHERE student_no = ? AND subject_cd = ? AND school_cd = ? AND no = ?";
+        }
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, test.getStudent().getNo());
+            statement.setString(2, test.getClassNum());
+            statement.setString(3, test.getSubject().getCd());
+            statement.setString(4, test.getSchool().getCd());
+            statement.setInt(5, test.getNo());
+            statement.setInt(6, test.getPoint());
+
+            result = statement.executeUpdate() > 0;
+        }
+
+        return result;
+    }
+
+    public boolean delete(List<Test> list) throws Exception {
+        boolean result = true;
+
+        try (Connection connection = getConnection()) {
+            connection.setAutoCommit(false);
+
+            for (Test test : list) {
+                if (!delete(test, connection)) {
+                    result = false;
+                    break;
+                }
+            }
+
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback();
+            }
+        }
+
+        return result;
+    }
+
+    private boolean delete(Test test, Connection connection) throws Exception {
+        boolean result;
+
+        String sql = "DELETE FROM test WHERE student_no = ? AND subject_cd = ? AND school_cd = ? AND no = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, test.getStudent().getNo());
+            statement.setString(2, test.getSubject().getCd());
+            statement.setString(3, test.getSchool().getCd());
+            statement.setInt(4, test.getNo());
+
+            result = statement.executeUpdate() > 0;
+        }
+
+        return result;
+    }
 }
-
