790e0fd9517f8efb00e84d0d3276f12215370730
admin <admin@LAPTOP-7CHGMKTV>
2024-07-17 12:25:27 +0900

    action -> main　フォルダ名変更

--- src/main/LoginExecuteAction.java
+++ src/main/LoginExecuteAction.java
@@ -1,4 +1,4 @@
-package action;
+package main;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
--- src/main/StudentCreateExecuteAction.java
+++ src/main/StudentCreateExecuteAction.java
@@ -1,4 +1,4 @@
-package action;
+package main;
 
 import java.io.IOException;
 import java.sql.Connection;
