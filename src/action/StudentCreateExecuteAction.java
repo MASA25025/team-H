5a8e34acbbd4a6d48575f9745c657b2890ca4fce
k_takemura <k_takemura@LAPTOP-AS7S4JRJ>
2024-07-17 11:12:04 +0900

    a

--- src/action/StudentCreateExecuteAction.java
+++ src/action/StudentCreateExecuteAction.java
@@ -63,9 +63,11 @@ public class StudentCreateExecuteAction extends Action {
 
         // データベース接続情報
         String jdbcUrl = "jdbc:h2:tcp://localhost/~/javasd";
-        String sql = "sa";
+        String dbUser = "sa";
+        String dbPassword = ""; // パスワードを設定してください
+        String sql = "SELECT COUNT(*) FROM students WHERE student_number = ?";
 
-        try (Connection conn = DriverManager.getConnection(jdbcUrl);
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, studentNumber);
@@ -84,11 +86,8 @@ public class StudentCreateExecuteAction extends Action {
         return isDuplicate;
     }
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+    }
 }
