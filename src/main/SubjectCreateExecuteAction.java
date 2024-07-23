d71b704ccfb800fb2de4b1fb97144596a0c80372
r_kanamori <r_kanamori@192.168.1.12>
2024-07-24 02:22:22 +0900

    これでいいのかしら

--- src/main/SubjectCreateExecuteAction.java
+++ src/main/SubjectCreateExecuteAction.java
@@ -20,23 +20,23 @@ public class SubjectCreateExecuteAction extends Action {
         String cd = req.getParameter("cd");
         String name = req.getParameter("name");
 
-        // Studentインスタンスの作成
+        // Subjectインスタンスの作成
         Subject subject = new Subject();
         subject.setCd(cd);
         subject.setName(name);
         subject.setSchool(teacher.getSchool());
 
-        // StudentDaoを使用して学生情報を保存
+        // SubjectDaoを使用して学生情報を保存
         SubjectDao subjectDao = new SubjectDao();
         boolean result = subjectDao.save(subject);
 
         if (result) {
             // 成功メッセージをリクエストに設定
-            req.setAttribute("message", "学生情報が登録されました。");
-            req.getRequestDispatcher("student_create_done.jsp").forward(req, resp);
+            req.setAttribute("message", "科目情報が登録されました。");
+            req.getRequestDispatcher("subject_create_done.jsp").forward(req, resp);
         } else {
             // エラーメッセージをリクエストに設定
-            req.setAttribute("error", "学生情報の登録に失敗しました。");
+            req.setAttribute("error", "科目情報の登録に失敗しました。");
         }
 
         // JSPページにフォワード
