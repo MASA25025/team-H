1f99e225827bbd5a56bb7d0a0f21d3ec472ae49c
r_kanamori <r_kanamori@LAPTOP-4RVDIL90>
2024-07-25 12:09:03 +0900

    つくた

--- src/main/SubjectDeleteAction.java
+++ src/main/SubjectDeleteAction.java
@@ -1,5 +1,29 @@
 package main;
 
-public class SubjectDeleteAction {
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp)throws Exception{
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		String cd = req.getParameter("cd");
+		Subject subjects = null;
+		SubjectDao subDao = new SubjectDao();
+
+		subjects = subDao.get(cd, teacher.getSchool());
+
+		req.setAttribute("subjects", subjects);
+
+		req.getRequestDispatcher("subject_delete.jsp").forward(req, resp);
+	}
 }
--- src/main/SubjectDeleteExecuteAction.java
+++ src/main/SubjectDeleteExecuteAction.java
@@ -1,5 +1,30 @@
 package main;
 
-public class SubjectDeleteExecuteAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+
+public class SubjectDeleteExecuteAction {
+	public void execute(HttpServletRequest req, HttpServletResponse resp)throws Exception{
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+        String cd = req.getParameter("cd");
+        String name = req.getParameter("name");
+		SubjectDao subDao = new SubjectDao();
+
+		Subject subject = new Subject();
+		subject.setCd(cd);
+		subject.setName(name);
+		subject.setSchool(teacher.getSchool());
+
+		boolean subjects = subDao.delete(subject);
+
+		req.setAttribute("subjects", subjects);
+
+		req.getRequestDispatcher("subject_delete.jsp").forward(req, resp);
+	}
 }
