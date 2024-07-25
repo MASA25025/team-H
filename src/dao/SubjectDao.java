6929bc88f473fdbb17e6106256a0c5de111ab873
r_kanamori <r_kanamori@LAPTOP-4RVDIL90>
2024-07-25 11:32:40 +0900

    a

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -85,8 +85,8 @@ public class SubjectDao extends DAO{
 			int count = 0;
 
 			try{
-				Subject old = get(subject.getCd(), subject.getSchool());
-				if ( old==null ){
+				Subject reg = get(subject.getCd(), subject.getSchool());
+				if ( reg==null ){
 //					学生が存在しなかった場合
 //					プリペアードステートメントにINSERT文をセット
 					statement = connection.prepareStatement(
@@ -103,6 +103,53 @@ public class SubjectDao extends DAO{
 					statement.setString(1, subject.getName());
 					statement.setString(5, subject.getCd());
 				}
+
+//				プリペアードステートメントの実行
+				count = statement.executeUpdate();
+			}catch (Exception e) {
+				// TODO: handle exception
+				throw e;
+			}finally {
+
+				if (statement !=null){
+					try{
+						statement.close();
+					}catch (SQLException sqle) {
+						// TODO: handle exception
+						throw sqle;
+					}
+				}
+				if (connection != null){
+					try{
+						connection.close();
+					}catch (SQLException sqle) {
+						// TODO: handle exception
+						throw sqle;
+					}
+				}
+			}
+
+			if(count >0){
+//				実行件数が1件以上ある場合
+				return true;
+			}else{
+//				実行件数が0件の場合
+				return false;
+			}
+	}
+
+		public boolean delete(Subject subject) throws Exception{
+			Connection connection = getConnection();
+			PreparedStatement statement = null;
+//			実行件数
+			int count = 0;
+
+			try{
+//					学生が存在した場合
+//					プリペアードステートメントにUPDATE文をセット
+					statement = connection.prepareStatement(
+							"delete from subject where cd=?");
+					statement.setString(1, subject.getCd());
 
 //				プリペアードステートメントの実行
 				count = statement.executeUpdate();
